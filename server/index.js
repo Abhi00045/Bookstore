9d0e95bc48e4e8bd0a17ec5471692d13e5ad55a8
abhi <rajidiabhinavreddy@gmail.com>
2025-03-02 00:31:19 +0530

    done with this mongoose shit

--- server/index.js
+++ server/index.js
@@ -2,17 +2,28 @@ const express = require('express')
 const app = express()
 const cors  = require('cors')
 const env = require('dotenv')
+const mongoose = require('mongoose');
 
-const port = process.env.port || 5190
+const port = process.env.PORT || 8000;
 
 //midddleware 
 app.use(cors());
 app.use(express.json());
 
+let MONGOURL = process.env.MONGO_URL;
+
+mongoose.connect(MONGOURL)
+  .then(()=>{
+    console.log("database connected successfully");
+    app.listen(port, () => {
+      console.log(`Example app listening on port ${port}`)
+    });
+  }).catch((error)=>{
+    console.log(error);
+    
+  })
+
 app.get('/', (req, res) => {
   res.send('Hello World!') 
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
