0ede25ed05a735804e9e990f8ec75d34be2ca637
abhi <rajidiabhinavreddy@gmail.com>
2025-03-23 01:51:18 +0530

    done with usestate

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,30 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import React from 'react';
 import axios from 'axios';
 
 const App = () => {
 
-  useEffect(()=>{
-    fetchData();
-  },[]);
+  // useEffect(()=>{
+  //   fetchData();
+  // },[]);
 
-  async function fetchData() {
-    const response = await axios.get("http://localhost:8000/get");
-    console.log(response);
+  // async function fetchData() {
+  //   const response = await axios.get("http://localhost:8000/get");
+  //   console.log(response);
+  // }
+  const [counter, setCounter]=useState(0);
+  const addValue=()=>{
+    setCounter(counter+1)
   }
-
+  const deleteValue=()=>{
+    setCounter(counter-1)
+  }
   return (
-    <div>App hs</div>
+    <>
+    <h1>Chai aur Abhi{counter} </h1>
+    <button onClick={addValue}>Addd</button>
+    <button onClick={deleteValue}>delete</button>
+    </>
   )
 }
 
